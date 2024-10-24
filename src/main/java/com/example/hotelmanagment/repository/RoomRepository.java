60a78663650b83e84578640e2ff33a3f6ed33a99
bekzod_mirzayev <bekzodmirzayev35@gmail.com>
2024-10-24 21:46:05 +0500

    some change

--- src/main/java/com/example/hotelmanagment/repository/RoomRepository.java
+++ src/main/java/com/example/hotelmanagment/repository/RoomRepository.java
@@ -2,6 +2,7 @@ package com.example.hotelmanagment.repository;
 
 import com.example.hotelmanagment.entity.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,5 +10,6 @@ import java.util.Optional;
 @Repository
 public interface RoomRepository extends JpaRepository<Room, Long> {
 
+    @Query("select r from Room r where r.roomNumber=:roomNumber")
     Optional<Room> findByRoomNumber(Integer roomNumber);
 }
